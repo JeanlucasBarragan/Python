730043a93442828640af5aba2a956c054090a97b
Jean Lucas Barragan <113623052+JeanlucasBarragan@users.noreply.github.com>
2023-03-07 16:31:44 -0300

    desafio tabuada
    solicitando ao usuário um número e mostrando a sua tabuda.

---
+++ exercicios/ex006.py
@@ -0,0 +1,6 @@
+metros = int(input('Digite algum valor: '))
+cen = metros * 100
+mili = metros * 1000
+print('Você digitou o valor {}' .format(metros))
+print('{} metros equivalem a {}cm' .format(metros, cen))
+print('{} metros equivalem a {}mm' .format(metros, mili))
